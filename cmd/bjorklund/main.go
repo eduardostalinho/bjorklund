094d4f5632bd6ba873582d856a7cc48ddaa74ea6
Eduardo Stalinho <eduardooc.86@gmail.com>
2020-05-13 16:53:35 +0200

    Add basic command tthat prints generated pattern.

---
+++ cmd/bjorklund/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/eduardostalinho/bjorklund"
+)
+
+
+func main() {
+	args := os.Args[1:]
+	
+	pulses, _ := strconv.Atoi(args[0])
+	slots, _ := strconv.Atoi(args[1])
+
+	rotation := 0
+	if len(args) > 2 {
+		rotation, _ = strconv.Atoi(args[2])
+	}
+	pattern, err := bjorklund.CreatePattern(pulses, slots, rotation)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(pattern)
+}
